d1bee83cc0106e0bc42ef91f0b362436231e30b8
burkero2 <>
2023-07-14 14:54:32 +0000

    Test Debug

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9fj^ew4mkf%t2mvht_r-%cgbm4*^81q64-=m=_k=nnqax0kqdr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-burkero2-django-blog-act-j1v06g55gg.us2.codeanyapp.com', '.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-burkero2-django-blog-act-j1v06g55gg.us2.codeanyapp.com', '.herokuapp.com']
 
 
 # Application definition
